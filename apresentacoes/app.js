ed9b9dc31806de35a31b938f3b72fab93efc5f1f
fonUFAL <78095594+fonufal@users.noreply.github.com>
2026-09-26 14:41:53 -0300

    Load fresh runtime config in student app

--- apresentacoes/app.js
+++ apresentacoes/app.js
@@ -6,7 +6,7 @@ import {
   getFirestore, doc, getDoc, collection, onSnapshot, runTransaction,
   serverTimestamp, setDoc
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-import { FIREBASE_CONFIG, MAIL_WEBAPP_URL, isFirebaseConfigured, isMailConfigured } from "./runtime-config.js";
+import { FIREBASE_CONFIG, MAIL_WEBAPP_URL, isFirebaseConfigured, isMailConfigured } from "./runtime-config.js?v=20260926-1737";
 import { TOPICS, TOPIC_MAP } from "./topics.js";
 
 const el = id => document.getElementById(id);
